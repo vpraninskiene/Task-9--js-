717abf3eba6a26e0ed14f2810b8aa53ca654aa9b
vpraninskiene <valentina.praninskiene@gmail.com>
2023-08-17 08:54:53 +0300

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus 3 savybėm ir 1 metodu:
+
+Savybės:
+title, director, budget
+Metodas: 
+wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
+------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive() {
+       return this.budget = this.budget >= 100_000_000 ? true : false;};    
+}
+
+const newMovie = new Movie("From the Dusk till Down", "K. Tarantino", 10_000_000);
+
+console.log(newMovie.wasExpensive());
